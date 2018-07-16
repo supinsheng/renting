9feca8ebae78b015e24160fdf7ee11a056692545
tangchunwei <2239991414@qq.com>
2018-07-16 18:25:37 +0800

    提交退租接口

--- app/Http/Controllers/InterfaceController.php
+++ app/Http/Controllers/InterfaceController.php
@@ -20,30 +20,62 @@ class InterfaceController extends Controller
         $xuzu->time = $data['xuzu_time'];
         $xuzu->state = '审核中';
         $xuzu->flow_number = date('YmdHis');
-//        if($xuzu->save()){
-//            $ret=[
-//                'errno'=>1,
-//                'errmsg'=>''
-//            ];
-////            状态码
-//            $status=200;
-//            return response($ret);
-//        }else {
-//            $ret=[
-//                'errno'=>0,
-//                'errmsg'=>'申请失败'
-//            ];
-////            状态码
-//            $status=201;
-//            return response($ret);
-//        }
+        if($xuzu->save()){
+            $ret=[
+                'errno'=>1,
+                'errmsg'=>''
+            ];
+//            状态码
+            $status=200;
+            return response($ret);
+        }else {
+            $ret=[
+                'errno'=>0,
+                'errmsg'=>'申请失败'
+            ];
+//            状态码
+            $status=201;
+            return response($ret);
+        }
 //        var_dump($req);
 //        $data= $req->getContent();
 //        $data = json_encode($data);
-        $xuzu->save();
+//        $xuzu->save();
 //        return $data['name'];
-        return  $xuzu->realname;
+//        return  $xuzu->realname;
 
+    }
+
+
+    //用户退租接口
+    public function  PostTuiZu(Request $req){
+        $data=json_decode($req->getContent(),TRUE);
+        $xuzu = new Xuzu;
+        $xuzu->realname = $data['name'];
+        $xuzu->cardId = $data['id_card'];
+        $xuzu->phone = $data['phone_num'];
+        $xuzu->address = $data['user_add'];
+        $xuzu->village = '建南小区';
+        $xuzu->state = '审核中';
+        $xuzu->state = '审核中';
+        $xuzu->flow_number = date('YmdHis');
+        if($xuzu->save()){
+            $ret=[
+                'errno'=>1,
+                'errmsg'=>''
+            ];
+//            状态码
+            $status=200;
+            return response($ret);
+        }else {
+            $ret=[
+                'errno'=>0,
+                'errmsg'=>'申请失败'
+            ];
+//            状态码
+            $status=201;
+            return response($ret);
+        }
     }
 
 }
