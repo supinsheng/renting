b8328a39983be0e53ce8c47500dd119c51bd8b8a
supinsheng <supinsheng@czxy.com>
2018-07-20 17:04:09 +0800

    更新后台代码

--- app/Http/Requests/XuzuRequest.php
+++ app/Http/Requests/XuzuRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class HouseholdRequest extends FormRequest
+class XuzuRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,21 +24,17 @@ class HouseholdRequest extends FormRequest
     public function rules()
     {
         return [
-            'username'=>' required | unique:households,username',
             'realname'=>' required ',
             'cardId'=>[
                 ' required ',
-                ' unique:households,cardId',
                 'regex:/(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx])|([1−9]\d5\d2((0[1−9])|(10|11|12))(([0−2][1−9])|10|20|30|31)\d2[0−9Xx])/'
             ],
             'phone'=>[
                 ' required ',
                 ' size:11',
-                ' unique:households,phone',
                 'regex:/((13[0-9])|(14[5,7])|(15[0-3,5-9])|(17[0,3,5-8])|(18[0-9])|166|198|199|(147))\d{8}/'
             ],
-            'start'=>'required',
-            'address'=>'required | unique:households,address',
+            'address'=>'required',
             'village'=>'required'
         ];
     }
@@ -46,20 +42,15 @@ class HouseholdRequest extends FormRequest
     public function messages(){
 
         return [
-
             'phone.required'=>'手机号 不能为空',
             'phone.size'=>'手机号为11位',
 
             'realname.required'=>'姓名 不能为空',
 
             'cardId.required'=>'身份证 不能为空',
-            'cardId.unique'=>'该身份证已经被使用',
             'cardId.regex'=>'请输入正确的身份证',
 
-            'start.required'=>'入住时间 不能为空',
-
             'address.required'=>'住址 不能为空',
-            'address.unique'=>'该房屋 已出租',
 
             'village.required'=>'小区 不能为空'
         ];
