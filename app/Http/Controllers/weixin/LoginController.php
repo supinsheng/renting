8927b733e2c6b0a9dbaf8fcd4a73e8646669c3a1
tangchunwei <2239991414@qq.com>
2018-08-30 16:41:16 +0800

    微信网页登录功能

---
+++ app/Http/Controllers/weixin/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\weixin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Model\Household;
+use App\Http\Requests\weixin\LoginRequest;
+
+class LoginController extends Controller
+{
+    public  function  login(){
+        return view('Weixin.login');
+    }
+    public  function  dologin(LoginRequest $req){
+        // 先通过手机号到数据库中查询用户的信息
+        $household = Household::where('username',$req->username)->first();
+        // 判断是否有这个账号
+        if($household)
+        {
+            if($req->isagree!='on'){
+                return back()->withErrors('请先同意建宁县公租房微信平台协议');
+            }
+            // 判断密码
+            // 表单中的密码：$req->password   （原始）
+            // 数据库的密码：$user->password （哈希之后 ）
+            // laravel中 Hash::check(原始，哈希之后)判断是否一致
+            if( $req->password   ==   $household->password   )
+            {
+                // 把用户常用的数据保存到SESSION（标记一下、打卡）
+                session([
+                    'id' => $household->id,
+                ]);
+                // 跳转
+                return redirect()->route('weixin_index');
+            }
+            else
+            {
+                // 密码错误
+                return back()->withErrors('密码错误！');
+            }
+        }
+        else
+        {
+            // 账号不存在
+            // 返回上一个页面，并把错误信保存到SESSION中，返回，在下一个页面中就可以使用 $errors 获取这个错误信息了
+            return back()->withErrors('账户不存在！');
+        }
+    }
+}
---
+++ resources/views/Weixin/login.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" />
+		<title>掌上公租房</title>
+		<link rel="stylesheet" type="text/css" href="/css/weixin/base.css"/>
+		<link rel="stylesheet" type="text/css" href="/css/weixin/login.css"/>
+	</head>
+	<body>
+		<div class="wrap">
+			<form action="{{ route('weixin_dologin') }}" id="test_form"  method="post">
+				{{csrf_field()}}
+				<div class="main">
+					<img src="/images/weixin/logo.png"/>
+					<input type="text" placeholder="会员账号" name="username" />
+					<input type="text" placeholder="会员密码" name="password" />
+                    @if($errors->any())
+                        <ul>
+                            @foreach($errors->all() as $e)
+                                <li style="color: red">{{$e}}</li>
+                            @endforeach
+                        </ul>
+                    @endif
+					<div class="xieyi">
+						<input type="checkbox" name="isagree" />我同意<a href="">《建宁县公租房微信平台协议》</a>
+					</div>
+					<div class="login" onclick="subform()">
+						登录
+					</div>
+				</div>
+			</form>
+		</div>
+	</body>
+</html>
+<script>
+//    点击登录提交表单
+    var form = document.getElementById('test_form');
+    function subform () {
+        form.submit();
+    }
+</script>
