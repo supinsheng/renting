aa04cdff1538a6a2e7845b01ad856c2488c55331
humie <hyp@czxy.com>
2018-11-23 23:16:22 +0800

    fsd

--- app/Http/Controllers/weixin/WxpayController.php
+++ app/Http/Controllers/weixin/WxpayController.php
@@ -38,6 +38,7 @@ class WxpayController extends Controller
         // return $obj;
         echo '<pre>';
         var_dump($data,$obj);
+        print $obj->{"openid"};
         die;
         $openid = $obj['openid'];
 
